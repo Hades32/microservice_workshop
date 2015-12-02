31b8319451f481d977db8dc70cba162d2b597326
Martin Rauscher <hades32@gmail.com>
2015-12-02 22:14:57 +0100

    Change NeedPacket to be a better example how to handle (unkown) json
    Not sure if you *wanted* people to make this lesson themselves, but the Ruby template is better suited to its actual usage, so I think it's better to save the attendees some precious time on that one.

--- java/src/main/java/com/microservices/rentaloffer/NeedPacket.java
+++ java/src/main/java/com/microservices/rentaloffer/NeedPacket.java
@@ -1,25 +1,45 @@
-package com.microservices.rentaloffer;
+package com.microservices.rentaloffer.need;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
+import java.lang.reflect.Type;
+import java.util.*;
+
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class NeedPacket {
 
-    public final String NEED = "car_rental_offer";
-    private final List<Object> solutions = new ArrayList<>();
+    private final static Gson gson = new Gson();
 
-    public String toJson() {
-        Map<String, Object> message = new HashMap<>();
-        message.put("json_class", NeedPacket.class.getName());
-        message.put("need", NEED);
-        message.put("solutions", solutions);
-        return new Gson().toJson(message);
+    private Map<String, Object> msg;
+
+    public NeedPacket(Map<String, Object> msg) {
+        this.msg = msg;
     }
 
-    public void proposeSolution(Object solution) {
-        solutions.add(solution);
+    public NeedPacket() {
+        msg = new HashMap<>();
+        msg.put("need","car_rental_offer");
+        msg.put("json_class", NeedPacket.class.getSimpleName());
+    }
+
+    public static NeedPacket fromJSON(String json){
+        Type mapType = new TypeToken<Map<String,Object>>() {}.getType();
+        Map<String,Object> packet = gson.fromJson(json, mapType);
+        return new NeedPacket(packet);
+    }
+
+    public String toJson() {
+        return gson.toJson(this.msg);
+    }
+
+    public void setUser(String user) {
+        msg.put("user", user);
+    }
+
+    public Collection<Object> getSolutions() {
+        if (!msg.containsKey("solutions"))
+            return null;
+        else 
+            return (Collection)msg.get("solutions");
     }
 }
